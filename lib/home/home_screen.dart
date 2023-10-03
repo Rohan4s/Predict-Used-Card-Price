70e9f2adc83fbc7bd10eb20cc77800df3d0e343c
Rohan4s <rohan22@student.sust.edu>
2023-10-03 23:33:49 +0600

    add prediction

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:predict_used_car_price/data/data.dart';
 import 'package:predict_used_car_price/styles/app_texts.dart';
+import 'package:http/http.dart' as http;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:predict_used_car_price/auth/auth.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,8 +17,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final User? user = Auth().currentUser;
+  final ValueNotifier<bool> fetching = ValueNotifier<bool>(false);
   final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
-  static const double tilePadding = 10;
+  static const double tilePadding = 20;
+  final TextStyle labelStyle = const TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.w300,
+    fontSize: 12,
+  );
+  final TextStyle floatingLabelStyle = const TextStyle(
+    color: Colors.white,
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+  );
+  final TextStyle selectedStyle = TextStyle(
+    color: Colors.amber[50],
+    fontSize: 12,
+    // fontWeight: FontWeight.w300,
+  );
+  final TextStyle submitTextStyle = TextStyle(
+    color: Colors.teal[200],
+    fontSize: 12,
+    // fontWeight: FontWeight.w300,
+  );
+  final ButtonStyle submitButtonStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey[900]!),
+    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18.0),
+        side: const BorderSide(color: Colors.white),
+      ),
+    ),
+  );
+  Color? dropDownColor = Colors.blueGrey[700];
+  Icon dropDownIcon = const Icon(
+    Icons.arrow_drop_down,
+    color: Colors.white,
+  );
 
   @override
   void initState() {
@@ -22,130 +65,291 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(AppTexts.appName, style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blue,
+        title: const Text(
+          AppTexts.appName,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+        backgroundColor: Colors.blueGrey[700],
         centerTitle: true,
-
       ),
       body: Container(
-        // color: Colors.grey[800],
-        height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          child: FormBuilder(
-            key: _formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    FormBuilderDropdown(
-                      name: AppTexts.modelName,
-                      decoration: const InputDecoration(
-                        labelText: AppTexts.modelLabel,
-                      ),
-                      items: ['a', 'b', 'c']
-                          .map((model) => DropdownMenuItem(
-                                value: model,
-                                child: Text(model),
-                              ))
-                          .toList(),
-                    ),
-                    const SizedBox(height: tilePadding),
-                    FormBuilderDropdown(
-                      name: AppTexts.brandName,
-                      decoration: const InputDecoration(
-                        labelText: AppTexts.brandLabel,
-                      ),
-                      items: ['b1', 'b2', 'b3']
-                          .map((brand) => DropdownMenuItem(
-                                value: brand,
-                                child: Text(brand),
-                              ))
-                          .toList(),
-                    ),
-                    const SizedBox(height: tilePadding),
-                    FormBuilderDropdown(
-                      name: AppTexts.yearName,
-                      decoration: const InputDecoration(
-                        labelText: AppTexts.yearLabel,
-                      ),
-                      items: ['y1', 'y2', 'y3']
-                          .map((modelYear) => DropdownMenuItem(
-                                value: modelYear,
-                                child: Text(modelYear),
-                              ))
-                          .toList(),
-                    ),
-                    const SizedBox(height: tilePadding),
-                    FormBuilderDropdown(
-                      name: AppTexts.bodyTypeName,
-                      decoration: const InputDecoration(
-                        labelText: AppTexts.bodyTypeLabel,
-                      ),
-                      items: ['y1', 'y2', 'y3']
-                          .map((bodyType) => DropdownMenuItem(
-                                value: bodyType,
-                                child: Text(bodyType),
-                              ))
-                          .toList(),
-                    ),
-                    const SizedBox(height: tilePadding),
-                    FormBuilderDropdown(
-                      name: AppTexts.transmissionName,
-                      decoration: const InputDecoration(
-                        labelText: AppTexts.transmissionLabel,
-                      ),
-                      items: ['y1', 'y2', 'y3']
-                          .map((transmission) => DropdownMenuItem(
-                                value: transmission,
-                                child: Text(transmission),
-                              ))
-                          .toList(),
-                    ),
-                    const SizedBox(height: tilePadding),
-                    FormBuilderDropdown(
-                      name: AppTexts.fuelTypeName,
-                      decoration: const InputDecoration(
-                        labelText: AppTexts.fuelTypeLabel,
-                      ),
-                      items: ['y1', 'y2', 'y3']
-                          .map((fuelType) => DropdownMenuItem(
-                                value: fuelType,
-                                child: Text(fuelType),
-                              ))
-                          .toList(),
-                    ),
-                    const SizedBox(height: tilePadding),
-                    FormBuilderTextField(
-                      name: AppTexts.engineCapacityName, // Field name used to access the value
-                      decoration: const InputDecoration(labelText: AppTexts.engineCapacityLabel),
-                      keyboardType: TextInputType.number,
-                    ),
-                    const SizedBox(height: tilePadding),
-                    FormBuilderTextField(
-                      name: AppTexts.millageName, // Field name used to access the value
-                      decoration: const InputDecoration(labelText: AppTexts.millageLabel),
-                      keyboardType: TextInputType.number,
-                    ),
-                    const SizedBox(height: tilePadding*2),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          //TODO: Call Api From here
-                          final formData = _formKey.currentState!.value;
-                          print('Form data: $formData');
-                        }
-                      },
-                      child: const Text(AppTexts.predictLabel),
-                    ),
-                  ],
+        color: Colors.blueGrey[900],
+        child: FormBuilder(
+          key: _formKey,
+          child: Center(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              children: <Widget>[
+                FormBuilderDropdown(
+                  icon: dropDownIcon,
+                  name: AppTexts.modelName,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  menuMaxHeight: MediaQuery.of(context).size.height * 0.5,
+                  dropdownColor: dropDownColor,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.modelLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  items: DataClass.models
+                      .map((model) => DropdownMenuItem(
+                            value: model,
+                            child: Text(model, style: selectedStyle),
+                          ))
+                      .toList(),
                 ),
-              ),
+                const SizedBox(height: tilePadding),
+                FormBuilderDropdown(
+                  icon: dropDownIcon,
+                  name: AppTexts.brandName,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  menuMaxHeight: MediaQuery.of(context).size.height * 0.5,
+                  dropdownColor: dropDownColor,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.brandLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  items: DataClass.brands
+                      .map((brand) => DropdownMenuItem(
+                            value: brand,
+                            child: Text(brand, style: selectedStyle),
+                          ))
+                      .toList(),
+                ),
+                const SizedBox(height: tilePadding),
+                FormBuilderDropdown(
+                  icon: dropDownIcon,
+                  name: AppTexts.yearName,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  menuMaxHeight: MediaQuery.of(context).size.height * 0.5,
+                  dropdownColor: dropDownColor,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.yearLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  items: DataClass.modelYears
+                      .map((modelYear) => DropdownMenuItem(
+                            value: modelYear,
+                            child: Text(modelYear, style: selectedStyle),
+                          ))
+                      .toList(),
+                ),
+                const SizedBox(height: tilePadding),
+                FormBuilderDropdown(
+                  icon: dropDownIcon,
+                  name: AppTexts.bodyTypeName,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  menuMaxHeight: MediaQuery.of(context).size.height * 0.5,
+                  dropdownColor: dropDownColor,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.bodyTypeLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  items: DataClass.bodyTypes
+                      .map((bodyType) => DropdownMenuItem(
+                            value: bodyType,
+                            child: Text(bodyType, style: selectedStyle),
+                          ))
+                      .toList(),
+                ),
+                const SizedBox(height: tilePadding),
+                FormBuilderDropdown(
+                  icon: dropDownIcon,
+                  name: AppTexts.transmissionName,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  menuMaxHeight: MediaQuery.of(context).size.height * 0.5,
+                  dropdownColor: dropDownColor,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.transmissionLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  items: DataClass.transmissions
+                      .map((transmission) => DropdownMenuItem(
+                            value: transmission,
+                            child: Text(transmission, style: selectedStyle),
+                          ))
+                      .toList(),
+                ),
+                const SizedBox(height: tilePadding),
+                FormBuilderDropdown(
+                  icon: dropDownIcon,
+                  name: AppTexts.fuelTypeName,
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                  ]),
+                  menuMaxHeight: MediaQuery.of(context).size.height * 0.5,
+                  dropdownColor: dropDownColor,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.fuelTypeLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  items: DataClass.fuelType
+                      .map((fuelType) => DropdownMenuItem(
+                            value: fuelType,
+                            child: Text(fuelType, style: selectedStyle),
+                          ))
+                      .toList(),
+                ),
+                const SizedBox(height: tilePadding),
+                FormBuilderTextField(
+                  name: AppTexts.engineCapacityName,
+                  // Field name used to access the value
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.numeric(),
+                    FormBuilderValidators.min(0),
+                  ]),
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.engineCapacityLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: tilePadding),
+                FormBuilderTextField(
+                  name: AppTexts.millageName,
+                  // Field name used to access the value
+                  validator: FormBuilderValidators.compose([
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.numeric(),
+                    FormBuilderValidators.min(0),
+                  ]),
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    labelText: AppTexts.millageLabel,
+                    contentPadding: const EdgeInsets.all(2),
+                    labelStyle: labelStyle,
+                    floatingLabelStyle: floatingLabelStyle,
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                const SizedBox(height: tilePadding),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: ElevatedButton(
+                      style: submitButtonStyle,
+                      onPressed: getData,
+                      child: Text(
+                        AppTexts.predictLabel,
+                        style: submitTextStyle,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: tilePadding,
+                ),
+                ValueListenableBuilder(
+                    valueListenable: fetching,
+                    builder: (context, value, child) {
+                      return value
+                          ? const Center(child: CircularProgressIndicator())
+                          : const SizedBox.shrink();
+                    }),
+              ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  void getData() async {
+    if (_formKey.currentState!.saveAndValidate()) {
+      try {
+        fetching.value = true;
+        final response = await http.post(
+          Uri.parse(AppTexts.apiURL),
+          body: {
+            AppTexts.modelName: _formKey.currentState!.fields[AppTexts.modelName]!.value,
+            AppTexts.brandName: _formKey.currentState!.fields[AppTexts.brandName]!.value,
+            AppTexts.yearName: _formKey.currentState!.fields[AppTexts.yearName]!.value,
+            AppTexts.bodyTypeName: _formKey.currentState!.fields[AppTexts.bodyTypeName]!.value,
+            AppTexts.transmissionName:
+                _formKey.currentState!.fields[AppTexts.transmissionName]!.value,
+            AppTexts.fuelTypeName: _formKey.currentState!.fields[AppTexts.fuelTypeName]!.value,
+            AppTexts.engineCapacityName:
+                _formKey.currentState!.fields[AppTexts.engineCapacityName]!.value,
+            AppTexts.millageName: _formKey.currentState!.fields[AppTexts.millageName]!.value,
+          },
+        );
+        if (response.statusCode == 200) {
+          fetching.value = false;
+          final int price = json.decode(response.body);
+          if (context.mounted) {
+            _showPrediction(context, price);
+          }
+        }
+      } catch (e) {
+        fetching.value = false;
+        print(e);
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Bad Gateway! Error: $e')),
+          );
+        }
+      }
+    } else {
+      print('invalid data');
+    }
+  }
+
+  void _showPrediction(BuildContext context, int price) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.blueGrey[900],
+          title: Center(
+              child: Text(
+            'Prediction: $price BDT',
+            style: const TextStyle(fontSize: 18, color: Colors.amber),
+          )),
+          actions: <Widget>[
+            Image.asset('assets/car.png'),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Center(child: Text('Close')),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/styles/app_texts.dart
+++ lib/styles/app_texts.dart
@@ -4,14 +4,14 @@ class AppTexts {
   AppTexts._();
   static const String appName = 'Predict Used Car Price';
 
-  static const String modelName = 'model';
+  static const String modelName = 'car_model';
   static const String brandName = 'brand';
-  static const String yearName = 'year';
-  static const String bodyTypeName = 'bodyType';
+  static const String yearName = 'model_year';
+  static const String bodyTypeName = 'body_type';
   static const String transmissionName = 'transmission';
-  static const String fuelTypeName = 'fuelType';
-  static const String engineCapacityName = 'engineCapacity';
-  static const String millageName = 'millage';
+  static const String fuelTypeName = 'fuel_type';
+  static const String engineCapacityName = 'engine_capacity';
+  static const String millageName = 'kilometers_run';
 
   static const String modelLabel = 'Select Car Model';
   static const String brandLabel = 'Select Brand';
@@ -22,5 +22,9 @@ class AppTexts {
   static const String engineCapacityLabel = 'Enter Engine Capacity';
   static const String millageLabel = 'Enter Millage';
   static const String predictLabel = 'PREDICT PRICE';
+
+  static const String apiURL = 'http://192.168.0.107:3000/get-prediction/';
+
+  // static const String apiURL = 'http://10.0.2.2:3000/get-prediction/'; // for emulator
 
 }
