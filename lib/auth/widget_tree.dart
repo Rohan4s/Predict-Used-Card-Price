70be998ef9a3f8fb3f9f4b8813b1812705dc0949
Rohan4s <rohan22@student.sust.edu>
2023-10-05 09:35:31 +0600

    add footer

---
+++ lib/auth/widget_tree.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:predict_used_car_price/auth/auth.dart';
+import 'package:predict_used_car_price/predict_price/predict_price_screen.dart';
+import 'package:predict_used_car_price/auth/login_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: Auth().authStateChanges,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const PredictPrice();
+          } else {
+            return const LoginPage();
+          }
+        });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:predict_used_car_price/home/home_screen.dart';
+import 'package:predict_used_car_price/auth/widget_tree.dart';
+import 'package:predict_used_car_price/predict_price/predict_price_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,8 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  const HomeScreen(),
+      home: const WidgetTree(),
     );
   }
 }
-
